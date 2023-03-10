00e6cf7366ad5b643f631784b4816e5ec550d583
ellohez <helenpjobs@gmail.com>
2023-03-10 14:53:22 +0000

    Actually completed class!

--- src/com/qa/helloworld/Blackjack.java
+++ src/com/qa/helloworld/Blackjack.java
@@ -2,14 +2,25 @@ package com.qa.helloworld;
 
 public class Blackjack {
 	public static void main(String[] args) {
+		System.out.println(playBlackjack(10, 21));
+		System.out.println(playBlackjack(20, 18));
+		System.out.println(playBlackjack(1, 22));
+		System.out.println(playBlackjack(22, 23));
 
 	}
 
 	static int playBlackjack(int a, int b) {
 
-//		if a and b > 21 - > return 0
-//		if a > b and < 22 -> return a
-//			else -> return b
+		if (a > 21 && b > 21) {
+			return 0;
+		}
 
+		if (a > 21) {
+			return b;
+		} else if (b > 21) {
+			return a;
+		}
+
+		return Math.max(a, b);
 	}
 }
