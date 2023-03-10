17aafbc437ad8915a8325276ae7378f78d29fd33
ellohez <helenpjobs@gmail.com>
2023-03-10 13:01:57 +0000

    Completed Blackjack class

---
+++ src/com/qa/helloworld/Blackjack.java
@@ -0,0 +1,15 @@
+package com.qa.helloworld;
+
+public class Blackjack {
+	public static void main(String[] args) {
+
+	}
+
+	static int playBlackjack(int a, int b) {
+
+//		if a and b > 21 - > return 0
+//		if a > b and < 22 -> return a
+//			else -> return b
+
+	}
+}
