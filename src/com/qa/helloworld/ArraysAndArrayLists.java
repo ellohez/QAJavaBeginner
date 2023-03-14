240dd17b7c9bcbd525a2db263ad0905646959f85
ellohez <helenpjobs@gmail.com>
2023-03-14 11:15:05 +0000

    New code for ArrayLists examples

--- src/com/qa/helloworld/ArraysAndArrayLists.java
+++ src/com/qa/helloworld/ArraysAndArrayLists.java
@@ -1,5 +1,8 @@
 package com.qa.helloworld;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArraysAndArrayLists {
@@ -9,7 +12,10 @@ public class ArraysAndArrayLists {
 //		array1();
 //		array2();
 //		addDigits();
-		System.out.println(intToString());
+//		System.out.println(intToString());
+//		System.out.println(intToStringExtended());
+
+		arrayListStuff();
 	}
 
 	static void array1() {
@@ -76,40 +82,76 @@ public class ArraysAndArrayLists {
 		}
 	}
 
+//	This is a bit of a disaster! With more time, I would rework
 	static String intToStringExtended() {
 
 		System.out.println("Please enter a number between 1 - 999: ");
 		Scanner scan = new Scanner(System.in);
 		int input = scan.nextInt();
-		
-		String[][] intToWord2D = {
-					{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}, 
-					{"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
-					{"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"}
-					};
 
-		if(input > 0 && input < 20) {
+		String[][] intToWord2D = { { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" },
+				{ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+						"nineteen" },
+				{ "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" } };
+
+		if (input > 0 && input < 20) {
 			return intToWord2D[input / 10][input % 10];
 		}
-		
-		else if(input >= 20 && input < 100) {
+
+		else if (input >= 20 && input < 100) {
 			String strNum = "";
 			strNum = strNum.concat(intToWord2D[2][input / 10] + " ");
 			strNum = strNum.concat(intToWord2D[0][input % 10]);
-			return strNum; 
-		}
-		else if(input >= 100 && input < 1000) {
+			return strNum;
+		} else if (input >= 100 && input < 1000) {
 			String strNum = "";
 			strNum = strNum.concat(intToWord2D[0][input / 100] + " hundred and ");
 			int remainder = input % 100;
-			if(remainder < 20) {
+			if (remainder < 20) {
 				return strNum.concat(intToWord2D[input / 10][input % 10]);
-			}
-			else {
+			} else {
 				strNum = strNum.concat(intToWord2D[2][remainder / 10] + " ");
 				return strNum.concat(intToWord2D[0][input % 10]);
 			}
-		}
-		else return "Invalid number";
+		} else
+			return "Invalid number";
 	}
+
+	static void arrayListStuff() {
+		List<String> colourList = new ArrayList<>(List.of("Red", "Yellow", "Green"));
+
+//		Add my favourite colour
+		colourList.add("Purple");
+//		and others...
+		colourList.add("Pink");
+		colourList.add("Blue");
+//		Get index of my favourite colour
+//		Boolean inList = colourList.contains("Purple");
+		int faveColIndex = colourList.indexOf("Purple");
+		System.out.println("Purple is number " + faveColIndex + " in the list");
+
+//		Enhanced for loop
+		for (String colour : colourList) {
+			System.out.println(colour);
+		}
+
+		if (faveColIndex > 0) {
+			System.out.println("First entry is: " + colourList.get(0));
+			Collections.swap(colourList, 0, faveColIndex);
+//			Where is my favourite colour now?
+			faveColIndex = colourList.indexOf("Purple");
+			System.out.println("Purple is number " + faveColIndex + " in the list");
+		}
+
+//		Sorting using Collections helper methods
+		Collections.sort(colourList);
+//		Sorted!! ;-)
+
+//		Normal for loop
+		for (int i = 0; i < colourList.size(); i++) {
+			System.out.println("Index " + i + " Element = " + colourList[i]);
+		}
+
+	}
+
 }
